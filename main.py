ae202c358212ee7af2c7d42903cb8b6749876aad
SarangSrikanth <122543645+SarangSrikanth@users.noreply.github.com>
2023-01-31 13:10:36 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,71 @@
+def on_button_pressed_a():
+    global Player1, rounds
+    Player1 += 1
+    basic.show_leds("""
+        . # # # .
+                . # . # .
+                . # # # .
+                . # . # .
+                . # . # .
+    """)
+    rounds += 1
+    updateScores()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global ties, rounds
+    ties += 1
+    basic.show_leds("""
+        # # # # #
+                . . # . .
+                . . # . .
+                . . # . .
+                . . # . .
+    """)
+    rounds += 1
+    updateScores()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global player2, rounds
+    player2 += 1
+    rounds += 1
+    basic.show_leds("""
+        # # # . .
+                # . . # .
+                # # # . .
+                # . . # .
+                # # # . .
+    """)
+    updateScores()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    reset()
+    basic.show_string("\"Shall we play a game\"")
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 def updateScores():
     OLED.clear()
-    OLED.write_string_new_line("Player1;" + str(Player1))
+    OLED.write_string_new_line("Player1;" + ("" + str(Player1)))
     OLED.new_line()
-    OLED.write_string_new_line("\"player2\";" + str(player2))
+    OLED.write_string_new_line("Player2" + ("" + str(player2)))
     OLED.new_line()
-    OLED.write_string_new_line("\"rounds" + str(rounds))
+    OLED.write_string_new_line("rounds" + ("" + str(rounds)))
     OLED.new_line()
-    OLED.write_string_new_line("\"ties\"" + str(ties))
-Player1 = 0
+    OLED.write_string_new_line("ties" + ("" + str(ties)))
+def reset():
+    global ties, rounds, player2, Player1
+    ties = 0
+    rounds = 0
+    player2 = 0
+    Player1 = 0
+    OLED.init(128, 64)
+    OLED.write_string_new_line("\"Shall we play a game\"")
+    basic.pause(2000)
+    updateScores()
 player2 = 0
-rounds = 0
-ties = 0
 ties = 0
 rounds = 0
-player2 = 0
 Player1 = 0
-OLED.init(128, 64)
-OLED.write_string_new_line("\"Shall we play a game\"")
-basic.pause(2000)
-updateScores()
+reset()
--- main.ts
+++ main.ts
@@ -1,22 +1,65 @@
+input.onButtonPressed(Button.A, function () {
+    Player1 += 1
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    rounds += 1
+    updateScores()
+})
+input.onButtonPressed(Button.AB, function () {
+    ties += 1
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    rounds += 1
+    updateScores()
+})
+input.onButtonPressed(Button.B, function () {
+    player2 += 1
+    rounds += 1
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # . .
+        `)
+    updateScores()
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+    basic.showString("\"Shall we play a game\"")
+})
 function updateScores () {
     OLED.clear()
-    OLED.writeStringNewLine("Player1;" + Player1)
+    OLED.writeStringNewLine("Player1;" + ("" + Player1))
     OLED.newLine()
-    OLED.writeStringNewLine("\"player2\";" + player2)
+    OLED.writeStringNewLine("Player2" + ("" + player2))
     OLED.newLine()
-    OLED.writeStringNewLine("\"rounds" + rounds)
+    OLED.writeStringNewLine("rounds" + ("" + rounds))
     OLED.newLine()
-    OLED.writeStringNewLine("\"ties\"" + ties)
+    OLED.writeStringNewLine("ties" + ("" + ties))
 }
-let Player1 = 0
+function reset () {
+    ties = 0
+    rounds = 0
+    player2 = 0
+    Player1 = 0
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("\"Shall we play a game\"")
+    basic.pause(2000)
+    updateScores()
+}
 let player2 = 0
-let rounds = 0
 let ties = 0
-ties = 0
-rounds = 0
-player2 = 0
-Player1 = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("\"Shall we play a game\"")
-basic.pause(2000)
-updateScores()
+let rounds = 0
+let Player1 = 0
+reset()
